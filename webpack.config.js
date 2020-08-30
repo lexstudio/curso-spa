6b6bc33405e3e52526ae0c207134ae5f535bc903
Alexander <alexander@War-machine.localdomain>
2020-08-29 23:31:19 -0500

    antes de conectar con travis

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -30,6 +31,12 @@ module.exports = {
         template: './public/index.html',
         filename: './index.html'
       }
-    )
+    ),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: './src/styles/styles.css',
+        to: ''
+      }],
+    })
   ]
 }
